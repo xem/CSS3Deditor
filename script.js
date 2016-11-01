b8469625fa0bcaa7dca0d4639bb084b07ff49f58
xem <maxime.euziere@gmail.com>
2016-11-01 11:27:04 +0100

    basic edition

--- script.js
+++ script.js
@@ -55,3 +55,53 @@ onload = perspective.onchange = perspective.oninput = function(){
     main.style.perspective = (5400 - perspective.value) + "px";
   }
 }
+
+
+// Add plane
+plane_auto_id = 0;
+current_plane = null
+current_plane_width = 0;
+current_plane_height = 0;
+current_plane_posX = 0;
+current_plane_posY = 0;
+current_plane_posZ = 0;
+current_plane_rotX = 0;
+current_plane_rotY = 0;
+current_plane_rotZ = 0;
+
+plane.onclick = function(){
+  scene.innerHTML += "<div id=plane" + plane_auto_id + " class='plane selected'>";
+  id.value = "plane" + plane_auto_id;
+  current_plane = top["plane" + plane_auto_id];
+  current_plane_width = 100;
+  w.value = current_plane_width;
+  current_plane.style.width = current_plane_width + "px";
+  current_plane_height = 100;
+  h.value = current_plane_height;
+  current_plane.style.height = current_plane_height + "px";
+  current_plane.style.background = "url(tileset.png) no-repeat";
+  current_plane.style.backgroundSize = "auto 100px";
+}
+
+
+// Edit
+w.oninput = w.onchange = function(){
+  current_plane_width = w.value;
+  current_plane.style.width = current_plane_width + "px";
+  current_plane.style.backgroundSize = "auto " + Math.max(w.value, h.value) + "px";
+}
+
+h.oninput = h.onchange = function(){
+  current_plane_height = h.value;
+  current_plane.style.height = current_plane_height + "px";
+  current_plane.style.backgroundSize = "auto " + Math.max(w.value, h.value) + "px";
+}
+
+posX.onchange = posX.oninput =
+posY.onchange = posY.oninput =
+posZ.onchange = posZ.oninput =
+rotX.onchange = rotX.oninput =
+rotY.onchange = rotY.oninput =
+rotZ.onchange = rotZ.oninput = function(){
+  current_plane.style.transform = "translateX(" + posX.value + "px) translateY(" + posY.value + "px)translateZ(" + posZ.value + "px) rotateX(" + rotX.value + "rad) rotateY(" + rotY.value + "rad) rotateZ(" + rotZ.value + "rad)";
+}
