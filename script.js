6991a7334562baa2e82b24d07af233c013d513c0
xem <maxime.euziere@gmail.com>
2016-11-02 20:07:39 +0100

    add a sprite

--- script.js
+++ script.js
@@ -56,8 +56,7 @@ onload = perspective.onchange = perspective.oninput = function(){
   }
 }
 
-
-// Add plane
+// New plane
 plane_auto_id = 0;
 current_plane = null
 current_plane_width = 0;
@@ -84,24 +83,94 @@ plane.onclick = function(){
 }
 
 
-// Edit
+// New sprite
+sprite_auto_id = 0;
+current_sprite = null
+current_sprite_width = 0;
+current_sprite_height = 0;
+current_sprite_posX = 0;
+current_sprite_posY = 0;
+current_sprite_posZ = 0;
+current_sprite_rotX = 0;
+current_sprite_rotY = 0;
+current_sprite_rotZ = 0;
+
+sprite.onclick = function(){
+  scene.innerHTML += "<div id=sprite" + sprite_auto_id + " class='sprite selected'>";
+  id.value = "sprite" + sprite_auto_id;
+  current_sprite = top["sprite" + sprite_auto_id];
+  sprites.push(current_sprite);
+  current_sprite_width = 100;
+  w.value = current_sprite_width;
+  current_sprite.style.width = current_sprite_width + "px";
+  current_sprite_height = 100;
+  h.value = current_sprite_height;
+  current_sprite.style.height = current_sprite_height + "px";
+  current_sprite.style.background = "url(tileset.png) no-repeat";
+  current_sprite.style.backgroundSize = "auto 100px";
+  rotX.style.display = "none";
+  rotY.style.display = "none";
+  rotX.disabled = true;
+  rotY.disabled = true;
+  edited_object = "sprite";
+}
+
+
+// Edit object
+edited_object = null;
+
+// Edit width
 w.oninput = w.onchange = function(){
-  current_plane_width = w.value;
-  current_plane.style.width = current_plane_width + "px";
-  current_plane.style.backgroundSize = "auto " + Math.max(w.value, h.value) + "px";
+  
+  // Edit a plane
+  if(edited_object == "plane"){
+    current_plane_width = w.value;
+    current_plane.style.width = current_plane_width + "px";
+    current_plane.style.backgroundSize = "auto " + Math.max(w.value, h.value) + "px";
+  }
+  
+  // Edit a sprite
+  if(edited_object == "sprite"){
+    current_sprite_width = w.value;
+    current_sprite.style.width = current_sprite_width + "px";
+    current_sprite.style.backgroundSize = "auto " + Math.max(w.value, h.value) + "px";
+  }
 }
 
+// Edit height
 h.oninput = h.onchange = function(){
-  current_plane_height = h.value;
-  current_plane.style.height = current_plane_height + "px";
-  current_plane.style.backgroundSize = "auto " + Math.max(w.value, h.value) + "px";
+  
+  // Edit a plane
+  if(edited_object == "plane"){
+    current_plane_height = h.value;
+    current_plane.style.height = current_plane_height + "px";
+    current_plane.style.backgroundSize = "auto " + Math.max(w.value, h.value) + "px";
+  }
+  
+  // Edit a sprite
+  if(edited_object == "sprite"){
+    current_sprite_height = h.value;
+    current_sprite.style.height = current_sprite_height + "px";
+    current_sprite.style.backgroundSize = "auto " + Math.max(w.value, h.value) + "px";
+  }
 }
 
+// Edit position and rotation
 posX.onchange = posX.oninput =
 posY.onchange = posY.oninput =
 posZ.onchange = posZ.oninput =
 rotX.onchange = rotX.oninput =
 rotY.onchange = rotY.oninput =
 rotZ.onchange = rotZ.oninput = function(){
-  current_plane.style.transform = "translateX(" + posX.value + "px) translateY(" + posY.value + "px)translateZ(" + posZ.value + "px) rotateX(" + rotX.value + "rad) rotateY(" + rotY.value + "rad) rotateZ(" + rotZ.value + "rad)";
+  if(edited_object == "plane"){
+    current_plane.style.transform = "translateX(" + posX.value + "px) translateY(" + posY.value + "px)translateZ(" + posZ.value + "px) rotateX(" + rotX.value + "rad) rotateY(" + rotY.value + "rad) rotateZ(" + rotZ.value + "rad)";
+  }
 }
+
+// Rotate sprites at each frame
+sprites = [];
+setInterval(function(){
+  for(i in sprites){
+    sprites[i].style.transform = "translateX(" + posX.value + "px) translateY(" + posY.value + "px)translateZ(" + posZ.value + "px) rotateX(" + (Math.PI / 2) + "rad) rotateY(" + (-camera_angle_z) + "rad) rotateZ(" + rotZ.value + "rad)";
+  }
+},33);
